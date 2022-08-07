62b93d3cc474942d3f4b06d949bbe85c09d60282
Duwe Aynab <mfaynab88@gmail.com>
2022-08-07 19:01:47 -0400

    completed header section.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import OUTSIDE from '../../assets/outside.png'
+import ME from '../../assets/me.png'
 import HeaderSocials from './HeaderSocials'
 
 
@@ -15,8 +15,8 @@ const Header = () => {
         <CTA />
         <HeaderSocials />
 
-        <div className="outside">
-          <img src={OUTSIDE} alt="outside" />
+        <div className="me">
+          <img src={ME} alt="me" />
         </div>
 
         <a href="#contact" className='scroll__down'>Scroll Down</a>
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,7 +7,7 @@ import {FiDribbble} from 'react-icons/fi'
 
 const HeaderSocials = () => {
   return (
-    <div className='header_socials'>
+    <div className='header__socials'>
         <a href='https://linkedin.com' target="_blank"><BsLinkedin/></a>
         <a href='https://Github.com' target="_blank"><GoMarkGithub/></a>
         <a href='https://dribbble.com' target="_blank"><FiDribbble/></a>
